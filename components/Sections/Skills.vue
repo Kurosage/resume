51a0c3fcf8a2a7f94e2a686a4bee8f494c03deec
Kurosage <asuna35981@gmail.com>
2024-03-17 15:34:45 +0300

    okey

---
+++ components/Sections/Skills.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="my-auto">
+    <h2 class="mb-5">{{ $t('skills.title') }}</h2>
+
+    <div class="subheading mb-3">{{ $t('skills.iconsTitle') }}</div>
+    <ul class="list-inline list-icons" style="text-align:center">
+      <!-- <li v-for="icon in title" :key="icon" class="list-inline-item">
+          <div :class="icon"></div>
+      </li> -->
+      <li v-for="(icon,text) in $t('skills.devIcons')" :key="icon" >
+        <!-- <p v-for="text in $t('skills.devIconsText')" :key="icon" class="list-inline-item"> -->
+            <i style="display: inline-block" :class="icon">{{text}}</i>
+        <!-- </p> -->
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script >
+
+export default{
+  setup(){
+   
+    return{
+   
+    }
+  } 
+}
+</script>
